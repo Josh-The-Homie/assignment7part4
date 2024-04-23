50284faa8b9b20f0e99969425eb244bbe2d75bec
Josh-Henderson <122053780+Josh-The-Homie@users.noreply.github.com>
2024-04-23 09:00:48 -0400

    Add files via upload

---
+++ Palendrome.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.util.Stack;
+/**
+ * @author Joshua Henderson
+ */
+/**
+ * The program simply creates a stack with the characters of the given phrase.
+ * Then it creates a string using the stack in reverse order. 
+ * Then it checks if the string before and after are equal.
+ */
+public class Palendrome {
+	public static void main(String[] args) {
+	    Stack<Character> stack = new Stack<Character>();
+		Scanner myPalendrome = new Scanner(System.in);
+		System.out.println("Enter Word");
+		String Palendrome = myPalendrome.nextLine();
+		for (int i=0;i<Palendrome.length();i++) {
+			stack.push(Palendrome.charAt(i));
+		}//end for loop
+		String reversePalendrome = "";
+		while (!stack.isEmpty()) {
+			reversePalendrome = reversePalendrome+stack.pop();
+		}//end while loop
+		if (Palendrome.equals(reversePalendrome)) {
+			System.out.println("Your phrase is a Palendrome");
+			
+		}
+		else {
+			System.out.println("Your phrase is not a Palendrome");
+		}//end if statement.
+		myPalendrome.close();
+	}//end main
+	
+		
+	}//end Palendrome
